d0e309c3e5d9e6df03e61b44cba68679bee271d8
Johanna Keïta <veille.technologique.jk@gmail.com>
2025-06-19 19:33:47 +0200

    Added PRIMARY KEY + CONSTRAINT FOREIGN KEY in order and order_product

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -16,7 +16,6 @@ CREATE TABLE IF NOT EXISTS user (
         phone_number VARCHAR(20) NOT NULL
 );
 
-
 CREATE TABLE IF NOT EXISTS product (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL UNIQUE,
@@ -27,23 +26,21 @@ CREATE TABLE IF NOT EXISTS product (
     poster_path VARCHAR(255) NOT NULL
 );
 
-
 CREATE TABLE IF NOT EXISTS `order` (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL,
-    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    total DECIMAL(10,2) NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES user(id)
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  total DECIMAL(10,2) NOT NULL,
+  user_id INT,
+  FOREIGN KEY (user_id) REFERENCES user(id)
 );
 
-
 CREATE TABLE IF NOT EXISTS order_product (
-    id INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT NOT NULL,
     product_id INT NOT NULL,
     quantity INT NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES `order`(id),
-    FOREIGN KEY (product_id) REFERENCES product(id)
+    PRIMARY KEY (order_id, product_id),
+    CONSTRAINT fk_order FOREIGN KEY (order_id) REFERENCES `order`(id),
+    CONSTRAINT fk_product FOREIGN KEY (product_id) REFERENCES product(id)
 );
 
 CREATE TABLE IF NOT EXISTS invoice (
@@ -53,9 +50,6 @@ CREATE TABLE IF NOT EXISTS invoice (
     date TIMESTAMP NOT NULL,
     FOREIGN KEY (order_id) REFERENCES `order`(id)
 );
-
-
-
 
 
 
